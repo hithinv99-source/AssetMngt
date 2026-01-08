95d7e7f098788e8fdb1d1ef08b780dcb283ea842
Vaibhav <2467635@cognizant.com>
2026-01-08 18:31:04 +0530

    added ef and user model

---
+++ AssetMngt/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AssetMngt.Models;
+namespace AssetMngt.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
+
---
+++ AssetMngt/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AssetMngt.Models
+{
+
+    public enum UserRole
+    {
+        Employee,
+        Manager,
+        Admin
+    }
+
+    public enum UserStatus
+    {
+        Active,
+        Inactive
+    }
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; } 
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public UserRole Role { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [StringLength(100)]
+        public string Department { get; set; }
+        [Required]
+        public UserStatus Status { get; set; }
+
+    }
+}
